f6ff75ab32090cb3480d5ee43bdf9db3ecd3cbde
11477872997 <1147872997@qq.com>
2022-02-02 14:05:22 +0800

    更新tabs

--- src/sonmodule/home/tabs.vue
+++ src/sonmodule/home/tabs.vue
@@ -1,48 +1,76 @@
 <template>
   <div class="tabs">
-    <a-tabs v-model:activeKey="activeKey" type="editable-card" @edit="onEdit" hideAdd>
-        <a-tab-pane v-for="pane in panes" :key="pane.key" :tab="pane.title" :closable="pane.closable"></a-tab-pane>
+    <a-tabs
+      v-model:activeKey="activeKey"
+      type="editable-card"
+      @edit="onEdit"
+      hideAdd
+    >
+      <a-tab-pane
+        v-for="pane in panes"
+        :key="pane.key"
+        :tab="pane.title"
+        :closable="pane.closable"
+      ></a-tab-pane>
     </a-tabs>
   </div>
 </template>
 
 <script>
-import { defineComponent,toRefs,reactive,watch  } from "vue";
-import { useRouter } from 'vue-router';
+import {
+  defineComponent,
+  toRefs,
+  reactive,
+  watch,
+  getCurrentInstance,
+} from "vue";
+import { useRouter } from "vue-router";
 export default defineComponent({
-    setup() {
-     const states = reactive({
+  setup() {
+    const states = reactive({
       //  初始化数据
       panes: [
-          {
+        {
           key: "/home",
           title: "首页",
           closable: false,
-      },
+        },
       ], //生成的集合数据
-      activeKey:'/home', //默认选中
-      collapsed:false,
+      activeKey: "/home", //默认选中
+      collapsed: false,
     });
     // 添加tabs
-    const add = (name,title) => {
- 
-      // states.panes.push({
-      //   title:title,
-      //   key:name,
-      // });
-     
+    const add = (name, title) => {
+        let panes = sessionStorage.getItem('panes');
+        // if(panes != null || panes != undefined){
+        //   sessionStorage.setItem("panes",JSON.stringify( states.panes));
+        //   };
+          let falg = true;
+          states.activeKey = name; //选中
+         states.panes.filter((item)=>{
+            // 判断是否存在
+              if(item.key == name){
+                  falg = false;
+              }
+        })
+      if (falg) {
+        states.panes.push({
+            title: title,
+            key: name,
+          });
+        }
     };
 
-  // 监听路由 
-    // let router = useRouter()
-    // watch(() =>router.currentRoute.value.path,(newValue,oldValue)=> {
-    //   let title = router.currentRoute.value.meta.title;
-    //    add(newValue,title);
-    // },{ immediate: true })
-   
- 
+    //监听路由
+    let router = useRouter();
+    watch( () => router.currentRoute.value.path, (newValue, oldValue) => {
+        let title = router.currentRoute.value.meta.title;
+         add(newValue, title);
+      },
+      { immediate: true }
+    );
 
-    const remove = targetKey => {
+    const remove = (targetKey) => {
       // let lastIndex = 0;
       // panes.value.forEach((pane, i) => {
       //   if (pane.key === targetKey) {
@@ -50,7 +78,6 @@ export default defineComponent({
       //   }
       // });
       // panes.value = panes.value.filter(pane => pane.key !== targetKey);
-
       // if (panes.value.length && activeKey.value === targetKey) {
       //   if (lastIndex >= 0) {
       //     activeKey.value = panes.value[lastIndex].key;
@@ -59,9 +86,9 @@ export default defineComponent({
       //   }
       // }
     };
-  	// 新增和删除页签的回调，在 type="editable-card" 时有效
+    // 新增和删除页签的回调，在 type="editable-card" 时有效
     const onEdit = (targetKey, action) => {
-      if (action === 'add') {
+      if (action === "add") {
         add();
       } else {
         remove(targetKey);
@@ -70,12 +97,10 @@ export default defineComponent({
 
     return {
       ...toRefs(states),
-      onEdit
+      onEdit,
     };
-   }
-})
+  },
+});
 </script>
 
-<style>
-
-</style>
+<style></style>
