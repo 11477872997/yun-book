b84cbfc17f90f93ae78ec1d530e60dac689d5ffd
11477872997 <1147872997@qq.com>
2022-02-10 09:41:09 +0800

    更新输出权限状态

--- api/login/login.js
+++ api/login/login.js
@@ -1,16 +1,16 @@
 // const {registerUser,insertUser}=require('../../sql/login/register');
 const logsUtil = require('../../config/logs');
-const { loginUser, INSERTlog,updateUser } = require('../../sql/login/login')
+const { loginUser, INSERTlog, updateUser } = require('../../sql/login/login')
 const md5 = require('../../util/md5');
 //引入jwt做token验证
 const jwt = require('jsonwebtoken');
 const dbconfig = require('../../config/db')
 const getNowTime = require('../../util/newdate');
 const verson = require('../../util/token'); //验证token
-const login = async (ctx, next) => {
+const login = async(ctx, next) => {
     try {
         let req = ctx.request.body;
-        if (!req.userName || !req.password) {   //密码账号不能为空
+        if (!req.userName || !req.password) { //密码账号不能为空
             ctx.response.status = 200;
             ctx.body = {
                 code: 0,
@@ -28,54 +28,57 @@ const login = async (ctx, next) => {
                 if (md5Pass == date[0].password) {
                     // console.log(ctx.headers['user-agent'])
                     //生成token，验证登录有效期
-                    const token = jwt.sign({ userId:date[0].userId }, dbconfig.jwtSecret, { expiresIn: dbconfig.expireTime });
-                //    记录登陆日志
-                    await INSERTlog(req.userName,date[0].userId,getNowTime(),ctx.ip,ctx.headers['user-agent']);
+                    const token = jwt.sign({ userId: date[0].userId }, dbconfig.jwtSecret, { expiresIn: dbconfig.expireTime });
+                    //    记录登陆日志
+                    await INSERTlog(req.userName, date[0].userId, getNowTime(), ctx.ip, ctx.headers['user-agent']);
                     // 首次登陆判断
-                    if(date[0].equipment == null || date[0].equipment == ''){   
-                         await updateUser(date[0].userId,token);  //更新登陆状态
+                    if (date[0].equipment == null || date[0].equipment == '') {
+                        await updateUser(date[0].userId, token); //更新登陆状态
+                        ctx.body = {
+                            code: 1,
+                            row: [{
+                                "token": token,
+                                "userName": req.userName,
+                                "power": date[0].power
+                            }],
+                            msg: '登陆成功',
+                        }
+                    } else {
+                        if (verson('Bearer ' + date[0].equipment) == false) {
+                            await updateUser(date[0].userId, token); //更新登陆状态
                             ctx.body = {
                                 code: 1,
-                                row:[{
-                                        "token":token,
-                                        "userName": req.userName,
+                                row: [{
+                                    "token": token,
+                                    "userName": req.userName,
+                                    "power": date[0].power
                                 }],
                                 msg: '登陆成功',
                             }
-                    }else{
-                       if(verson('Bearer '+ date[0].equipment) == false){
-                        await updateUser(date[0].userId,token);  //更新登陆状态
-                            ctx.body = {
-                                code: 1,
-                                row:[{
-                                        "token":token,
-                                        "userName": req.userName,
-                                }],
-                                msg: '登陆成功',
-                            }
-                       }else{
-                           if(req.zt == 0){
-                            await updateUser(date[0].userId,token);  //更新登陆状态
+                        } else {
+                            if (req.zt == 0) {
+                                await updateUser(date[0].userId, token); //更新登陆状态
                                 ctx.body = {
                                     code: 1,
-                                    row:[{
-                                            "token":token,
-                                            "userName": req.userName,
+                                    row: [{
+                                        "token": token,
+                                        "userName": req.userName,
+                                        "power": date[0].power
                                     }],
                                     msg: '登陆成功',
                                 }
-                           }else{
-                            if(date[0].equipment != token){
-                                ctx.body = {
-                                    code: 0,
-                                    zt:1,
-                                    msg: '登录的token与访问token不一致'
+                            } else {
+                                if (date[0].equipment != token) {
+                                    ctx.body = {
+                                        code: 0,
+                                        zt: 1,
+                                        msg: '登录的token与访问token不一致'
+                                    }
                                 }
                             }
-                           }
-                          
-                       }
-                             
+
+                        }
+
                     }
                 } else {
                     ctx.body = {
@@ -85,14 +88,14 @@ const login = async (ctx, next) => {
                 }
             }
         }
-        logsUtil.logResponse(ctx, req);	  //记录响应日志
+        logsUtil.logResponse(ctx, req); //记录响应日志
     } catch (err) {
         ctx.response.status = 500;
         ctx.body = {
             code: 0,
             msg: '服务器跑了'
         }
-        logsUtil.logError(ctx, err);//记录异常日志
+        logsUtil.logError(ctx, err); //记录异常日志
     }
 
 }
